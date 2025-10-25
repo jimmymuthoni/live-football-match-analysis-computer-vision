97cab9fee9ae76b7065b102b97478cfec88a5635
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-10-25 16:59:05 +0300

    methods to detect and track players

---
+++ trackers/tracker.py
@@ -0,0 +1,81 @@
+from ultralytics import YOLO
+import supervision as sv
+import pickle
+import os
+
+class Tracker:
+    def __init__(self, model_path):
+        self.model = YOLO(model_path)
+        self.tracker = sv.ByteTrack()
+
+    def detect_frames(self, frames):
+        """method to detect frames in the video input"""
+        batch_size = 20
+        detections = []
+        for i in range(0, len(frames), batch_size):
+            detections_batch = self.model.predict(frames[i:i+batch_size],conf=0.1)
+            detections += detections_batch
+        return detections
+
+    def get_object_tracks(self, frames, read_from_stub=False, stub_path=None):
+        """method to track objects"""
+
+        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+            with open(stub_path, 'rb') as f:
+                tracks = pickle.load(f)
+            return tracks
+        
+        detections = self.detect_frames(frames)
+
+        tracks = {
+            "players":[],
+            "referees":[],
+            "ball":[]
+        }
+
+        for frame_num,detection in enumerate(detections):
+            cls_names = detection.names
+            cls_names_inv = {v:k for k,v in cls_names.items()}
+            print(cls_names)
+            detection_supervision = sv.Detections.from_ultralytics(detection)
+
+            #convert goolkeeper to layer object
+            for object_indx, class_id in enumerate(detection_supervision.class_id):
+                if cls_names[class_id] == 'goalkeeper':
+                    detection_supervision.class_id[object_indx] = cls_names_inv['player']
+
+            """Track objects"""
+            detections_with_tracks = self.tracker.update_with_detections(detection_supervision)
+
+            tracks['players'].append({})
+            tracks['referees'].append({})
+            tracks['ball'].append({})
+
+            for frame_detection in detections_with_tracks:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+                track_id = frame_detection[4]
+
+                if cls_id == cls_names_inv['player']:
+                    tracks['players'][frame_num][track_id] = {"bbox":bbox}
+
+                if cls_id == cls_names_inv['referee']:
+                    tracks['referees'][frame_num][track_id] = {"bbox":bbox}
+
+            for frame_detection in detection_supervision:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+
+                if cls_id == cls_names_inv['ball']:
+                    tracks['ball'][frame_num][1] = {"bbox":bbox}
+
+        if stub_path is not None:
+            with open(stub_path, 'wb') as f:
+                pickle.dump(tracks, f)
+    
+        return tracks
+
+
+
+
+
