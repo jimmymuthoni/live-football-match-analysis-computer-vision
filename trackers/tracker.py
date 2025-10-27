e523c108b30e2915b5ebdf30df8615dd1cf194b9
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-10-27 18:26:26 +0300

    function to draw ellipse around the player

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -1,7 +1,11 @@
 from ultralytics import YOLO
 import supervision as sv
 import pickle
+from utils.bbox_utils import get_bbox_width,get_center_of_bbox
 import os
+import sys
+import cv2
+sys.path.append('../')
 
 class Tracker:
     def __init__(self, model_path):
@@ -75,6 +79,37 @@ class Tracker:
     
         return tracks
 
+    def draw_elipse(self,frame,bbox,color,track_id):
+        y2 = int(bbox[3])
+        x_center,_ = get_center_of_bbox(bbox)
+        width = get_bbox_width(bbox)
+        cv2.ellipse(frame,
+                    center=(x_center,y2),
+                    axes=(int(width),int(0.35*width)),
+                    angle=0.0,
+                    startAngle=45,
+                    endAngle = 235,
+                    color = color,
+                    thickness = 2,
+                    lineType = cv2.LINE_4
+        )
+
+        return frame
+    
+
+
+    def draw_anotations(self,video_frames,tracks):
+        output_video_frames = []
+        for frame_num,frame in enumerate(video_frames):
+            frame = frame.copy()
+
+            player_dict = tracks['players'][frame_num]
+            ball_dict = tracks['ball'][frame_num]
+            referee_dict = tracks['referees'][frame_num]
+
+            #draw elispe around the player
+            for track_id,player in player_dict.items():
+                frame = self.draw_elipse(frame, player['bbox'],(0,0,255),track_id)
 
 
 
