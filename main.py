8a8063fd165a7efffbe379e89ea2f12644477529
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-11-04 16:53:48 +0300

    added team asiigner module

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from utils.video_utils import read_video, save_video
 import os
 os.makedirs("output_videos", exist_ok=True)
 from trackers.tracker import Tracker
+from team_assigner.team_assigner import TeamAssigner
 import cv2
 
 def main():
@@ -10,15 +11,18 @@ def main():
     
     tracker = Tracker("models/best.pt")
     tracks = tracker.get_object_tracks(video_frames,read_from_stub=True, stub_path="stubs/track_stubs.pkl")
-
-    #saving and cropping image of a player
-    for track_id,player in tracks['players'][0].items():
-        bbox = player['bbox']
-        frame = video_frames[0]
-        cropped_image = frame[int(bbox[1]):int(bbox[3]),int(bbox[0]):int(bbox[2])]
-        cv2.imwrite(f"ouput_videos/cropped_image.jpg", cropped_image)
-        break
     
+    #assign player teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0],tracks['players'][0])
+
+    for frame_num, player_track in enumerate(tracks["players"]):
+        for player_id,track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num], track['bbox'],player_id)
+            tracks['players'][frame_num][player_id]['team'] = team
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
+
     #draw ouput
     output_video_frames = tracker.draw_anotations(video_frames, tracks)
 
