cbb22e5c0f2bf4189aba609ea2e71c788ecd8367
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-11-08 19:39:49 +0300

    entry point

--- main.py
+++ main.py
@@ -1,54 +1,81 @@
-from utils.video_utils import read_video, save_video
+from utils import read_video, save_video
+from trackers import Tracker
 import os
-os.makedirs("output_videos", exist_ok=True)
-from trackers.tracker import Tracker
-from player_ball_assigner.player_ball_assigner import PlayerBallAssigner
-from team_assigner.team_assigner import TeamAssigner
-import cv2
 import numpy as np
+os.makedirs("output_videos", exist_ok=True)
+from team_assigner import TeamAssigner
+from player_ball_assigner import PlayerBallAssigner
+from camera_movement.camera_move_estinamtor import CameraMovementEstimator
+from view_transformer import ViewTransformer
+from speed_and_distance_estimator.speed_distance_estimator import SpeedAndDistance_Estimator
+
 
 def main():
-    #read video
     video_frames = read_video('input_video_data/soccer_video_4.mp4')
-    
-    tracker = Tracker("models/best.pt")
-    tracks = tracker.get_object_tracks(video_frames,read_from_stub=True, stub_path="stubs/track_stubs.pkl")
-
+    tracker = Tracker('models/best.pt')
     #interpolate ball position
-    tracks['ball'] = tracker.interpolate_ball_position(tracks["ball"])
-    
-    #assign player teams
-    team_assigner = TeamAssigner()
-    team_assigner.assign_team_color(video_frames[0],tracks['players'][0])
+    tracks = tracker.get_object_tracks(video_frames,
+                                       read_from_stub=True,
+                                       stub_path='stubs/track_stubs.pkl')
+    # Get object positions 
+    tracker.add_position_to_tracks(tracks)
 
-    for frame_num, player_track in enumerate(tracks["players"]):
-        for player_id,track in player_track.items():
-            team = team_assigner.get_player_team(video_frames[frame_num], track['bbox'],player_id)
-            tracks['players'][frame_num][player_id]['team'] = team
+    # camera movement estimator
+    camera_movement_estimator = CameraMovementEstimator(video_frames[0])
+    camera_movement_per_frame = camera_movement_estimator.get_camera_movement(video_frames,
+                                                                                read_from_stub=True,
+                                                                                stub_path='stubs/camera_movement_stub.pkl')
+    camera_movement_estimator.add_adjust_positions_to_tracks(tracks,camera_movement_per_frame)
+
+
+    # View Trasnformer
+    view_transformer = ViewTransformer()
+    view_transformer.add_transformed_position_to_tracks(tracks)
+
+    # Interpolate Ball Positions
+    tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
+
+    # Speed and distance estimator
+    speed_and_distance_estimator = SpeedAndDistance_Estimator()
+    speed_and_distance_estimator.add_speed_and_distance_to_tracks(tracks)
+
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
+    
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
             tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
 
-
-    #assign ball aquisition
-    player_assigner = PlayerBallAssigner()
-    team_ball_control = []
-    for frame_num,player_track in enumerate(tracks['players']):
+    
+    # Assign Ball Aquisition
+    player_assigner =PlayerBallAssigner()
+    team_ball_control= []
+    for frame_num, player_track in enumerate(tracks['players']):
         ball_bbox = tracks['ball'][frame_num][1]['bbox']
-        assigned_player = player_assigner.assign_ball_to_player(player_track,ball_bbox)
+        assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
+
         if assigned_player != -1:
             tracks['players'][frame_num][assigned_player]['has_ball'] = True
-            team_ball_control.append(tracks['player'][frame_num][assigned_player]['team'])
-
+            team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
         else:
             team_ball_control.append(team_ball_control[-1])
-    team_ball_control = np.array(team_ball_control)
+    team_ball_control= np.array(team_ball_control)
 
 
-
-    #draw ouput
-    output_video_frames = tracker.draw_anotations(video_frames, tracks, team_ball_control)
-
-    #save video
+    #Draw object Tracks
+    output_video_frames = tracker.draw_annotations(video_frames, tracks,team_ball_control)
+    #Draw Camera movement
+    output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames,camera_movement_per_frame)
+      #Draw Speed and Distance
+    speed_and_distance_estimator.draw_speed_and_distance(output_video_frames,tracks)
+    #Save video
     save_video(output_video_frames, 'output_videos/output_video.avi')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
